66337f8c0af1047df4c03d66b0ed961b5aca0755
Saif <ayeshsaif367@gmail.com>
2024-07-15 22:36:46 +0300

    test commit

--- Website/settings.py
+++ Website/settings.py
@@ -51,18 +51,19 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Website.urls'
 
-CORS_ALLOWED_ORIGINS = [
-'http://127.0.0.1:3000',
-'http://localhost:5173',
-'http://127.0.0.1:8000',
-'http://localhost:8000'
-]
-CSRF_TRUSTED_ORIGINS = [
-'http://127.0.0.1:3000',
-'http://localhost:5173',
-'http://127.0.0.1:8000',
-'http://localhost:8000'
-]
+if DEBUG:
+    CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:5173',
+    'http://127.0.0.1:8000',
+    'http://localhost:8000'
+    ]
+    CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:5173',
+    'http://127.0.0.1:8000',
+    'http://localhost:8000'
+    ]
 
 CORS_ALLOW_METHODS = (
     "DELETE",
