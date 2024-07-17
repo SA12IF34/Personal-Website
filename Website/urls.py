fc448513cee3896d608775b6c974f20b806aa25a
Saif <ayeshsaif367@gmail.com>
2024-07-17 10:02:42 +0300

    commit

--- Website/urls.py
+++ Website/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.views.generic import TemplateView
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='dist/index.html')),
-    path('apis/', include('app.urls'))
-
+    path('apis/', include('app.urls')),
+    re_path(r'^mediafiles/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
